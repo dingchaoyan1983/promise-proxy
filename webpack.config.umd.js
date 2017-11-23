17080d6a8cfd16833f882306e22844a6184bf901
Dane <dane.ding@activenetwork.com>
2017-11-23 14:22:50 +0800

    Update webpack.config.umd.js

--- webpack.config.umd.js
+++ webpack.config.umd.js
@@ -1,5 +1,15 @@
 const path = require('path');
 const webpack = require('webpack');
+const fs = require('fs');
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function(x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function(mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
 
 module.exports = {
   entry: "./src/index.js",
